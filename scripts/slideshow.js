f4157fce726f80d5424261654710d6ded32119f3
jmewong <jmw488@cornell.edu>
2016-11-25 14:32:35 -0500

    imagecredits

--- scripts/slideshow.js
+++ scripts/slideshow.js
@@ -1,8 +1,17 @@
 
 /* */
+// fun.jpg from https://www.flickr.com/photos/106103661@N02/15266538019/in/album-72157648366747526/
+// fun1.jpg from https://www.flickr.com/photos/106103661@N02/15266598780/in/album-72157648366747526/
+// fun2.jpg from https://www.flickr.com/photos/106103661@N02/10396929706/in/album-72157648008079417/
 var fun_imagearray = ["fun.jpg", "fun1.jpg", "fun2.jpg"];
+// market.jpg, market1.jpg, market2.jpg by Jamie Wong
 var market_imagearray = ["market.jpg", "market1.jpg", "market2.jpg"];
+// food.jpg from https://www.flickr.com/photos/106103661@N02/21839487978/in/album-72157657274645113/
+// food1.jpg, food2.jpg by Jamie Wong
 var food_imagearray = ["food.jpg", "food1.jpg", "food2.jpg"];
+// craft.jpg from https://www.flickr.com/photos/106103661@N02/10396902815/in/album-72157648008079417/
+// craft1.jpg from 	https://www.flickr.com/photos/106103661@N02/15453293375/in/album-72157648366747526/
+// craft2.jpg by Jamie Wong
 var craft_imagearray = ["craft.jpg", "craft1.jpg", "craft2.jpg"];
 var curIndex = 0;
 var imgDuration = 10000;
