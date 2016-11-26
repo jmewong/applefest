b050fa4fe555743435bcdd2eb72bc9c81622d900
jmewong <jmw488@cornell.edu>
2016-11-26 08:30:35 -0500

    updatedhover

--- scripts/slideshow.js
+++ scripts/slideshow.js
@@ -1,37 +1,53 @@
 
-/* */
+/* variables and function */
 // fun.jpg from https://www.flickr.com/photos/106103661@N02/15266538019/in/album-72157648366747526/
 // fun1.jpg from https://www.flickr.com/photos/106103661@N02/15266598780/in/album-72157648366747526/
 // fun2.jpg from https://www.flickr.com/photos/106103661@N02/10396929706/in/album-72157648008079417/
-var fun_imagearray = ["fun.jpg", "fun1.jpg", "fun2.jpg"];
+var fun_imgarray = ["fun.jpg", "fun1.jpg", "fun2.jpg"];
 // market.jpg, market1.jpg, market2.jpg by Jamie Wong
-var market_imagearray = ["market.jpg", "market1.jpg", "market2.jpg"];
+var market_imgarray = ["market.jpg", "market1.jpg", "market2.jpg"];
 // food.jpg from https://www.flickr.com/photos/106103661@N02/21839487978/in/album-72157657274645113/
 // food1.jpg, food2.jpg by Jamie Wong
-var food_imagearray = ["food.jpg", "food1.jpg", "food2.jpg"];
+var food_imgarray = ["food.jpg", "food1.jpg", "food2.jpg"];
 // craft.jpg from https://www.flickr.com/photos/106103661@N02/10396902815/in/album-72157648008079417/
-// craft1.jpg from 	https://www.flickr.com/photos/106103661@N02/15453293375/in/album-72157648366747526/
+// craft1.jpg from https://www.flickr.com/photos/106103661@N02/15453293375/in/album-72157648366747526/
 // craft2.jpg by Jamie Wong
-var craft_imagearray = ["craft.jpg", "craft1.jpg", "craft2.jpg"];
-var curIndex = 0;
+var craft_imgarray = ["craft.jpg", "craft1.jpg", "craft2.jpg"];
+var showIndex = 1;
 var imgDuration = 10000;
 
-function slideShow(id, imgarray) {
+function slideShow() {
     'use strict';
-    document.getElementById(id).className += "fadeOut";
-    setTimeout(function () {
-        document.getElementById(id).src = 'images/' + imgarray[curIndex];
-        document.getElementById(id).className = "";
-    }, 1000);
-    curIndex++;
-    if (curIndex === imgarray.length) { curIndex = 0; }
-    setTimeout(function () {
-        slideShow(id, imgarray)
-    }, imgDuration);
-}
-slideShow('fun_image', fun_imagearray);
-slideShow('market_image', market_imagearray);
-slideShow('food_image', food_imagearray);
-slideShow('craft_image', craft_imagearray);
 
-$("funanc").trigger('click');
+    /* fade out the old images by adding the fadeOut effect */
+    document.getElementById('fun_image').className += "fadeOut";
+    document.getElementById('market_image').className += "fadeOut";
+    document.getElementById('food_image').className += "fadeOut";
+    document.getElementById('craft_image').className += "fadeOut";
+
+    /* after 1 second, show the new row of images */
+    setTimeout(function () {
+        document.getElementById('fun_image').src = 'images/' + fun_imgarray[showIndex];
+        document.getElementById('fun_image').className = "";
+
+        document.getElementById('market_image').src = 'images/' + market_imgarray[showIndex];
+        document.getElementById('market_image').className = "";
+
+        document.getElementById('food_image').src = 'images/' + food_imgarray[showIndex];
+        document.getElementById('food_image').className = "";
+
+        document.getElementById('craft_image').src = 'images/' + craft_imgarray[showIndex];
+        document.getElementById('craft_image').className = "";
+    }, 1000);
+
+    /* adjust the index to show next image row */
+    showIndex++;
+    if (showIndex === fun_imgarray.length) { showIndex = 0; }
+
+    /* after 10 seconds, call slideShow again to show the next row of images */
+    setTimeout(slideShow, imgDuration);
+}
+
+/* after the page is loaded for 10 seconds, start the images slide show */
+setTimeout(slideShow, imgDuration);
+
